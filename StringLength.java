5efaee66e90a2dad89013c0455191d2fcc620408
Aryan Karna <231492473+VOID-088@users.noreply.github.com>
2025-10-15 14:02:32 +0530

    Add StringLength class to calculate string length

---
+++ StringLength.java
@@ -0,0 +1,12 @@
+public class StringLength {
+    public static void main(String[] args) {
+        // Create a string variable
+        String greeting = "Hello, World!";
+
+        // Use the length() method to get the number of characters
+        int length = greeting.length();
+
+        // Print the length to the console
+        System.out.println("The length of the string is: " + length); // Output will be 13
+    }
+}
